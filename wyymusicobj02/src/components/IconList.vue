f71424e790d1823f2ba8fa796d24b92dc82744d1
Fengchen <2409457584@qq.com>
2023-05-25 17:57:19 +0800

    2023年5月25日17点55分

---
+++ wyymusicobj02/src/components/IconList.vue
@@ -0,0 +1,15 @@
+<template>
+	<div id="">
+		iconlist
+	</div>
+</template>
+
+<script>
+	export default {
+	  name: 'iconlist',
+	  
+	}
+</script>
+
+<style>
+</style>
---
+++ wyymusicobj02/src/components/MusicList.vue
@@ -0,0 +1,15 @@
+<template>
+	<div id="">
+		musiclist
+	</div>
+</template>
+
+<script>
+	export default {
+	  name: 'musiclist',
+	  
+	}
+</script>
+
+<style>
+</style>
